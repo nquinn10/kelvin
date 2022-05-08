4c2312d31de4e25c603c497b27679df7ab163274
nquinn10 <104316408+nquinn10@users.noreply.github.com>
2022-05-07 20:36:09 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// creating constant kelvin value 
+const kelvin = 0;
+
+// celsisus variable calculation
+var celsius = kelvin - 273;
+
+//fahrenheit conversion
+// var fahrenheit = celsius * (9/5) + 32;
+
+//decimal specification
+var fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
